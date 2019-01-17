6ec1ad4fabdb1d27d2c5898924f157a46b6d86ed
Kyle Kratt <kylekratt@gmail.com>
2019-01-17 01:17:35 -0500

    Changed background image so it works with inline styling

--- src/components/FriendCard/index.js
+++ src/components/FriendCard/index.js
@@ -1,9 +1,12 @@
 import React from "react";
 import "./style.css";
 
+const imgStyle = {
+  backgroundImage: 'url(' + process.env.PUBLIC_URL + '/assets/images/pokeball.jpg)'
+}
 function FriendCard(props) {
   return (
-    <img onClick = {() => props.check(props.id)} alt="" className="border border-dark m-3 p-3 rounded" src={process.env.PUBLIC_URL + props.image} width="200px" height="200px"/>
+    <img onClick = {() => props.check(props.id)} alt="" className="border border-dark m-3 p-3 rounded" style={imgStyle} src={props.image} width="200px" height="200px"/>
   );
 }
 
