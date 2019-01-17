bcae12dfb792e9be1005836dcb0b11188d33d900
Kyle Kratt <kylekratt@gmail.com>
2019-01-17 00:11:48 -0500

    Updates

---
+++ precache-manifest.b54038da319bfced7f1674e76ddf5db3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6acb6692e4ee25941da6",
+    "url": "/clicky-game/static/js/runtime~main.6acb6692.js"
+  },
+  {
+    "revision": "d83250e02c17b2da2ba3",
+    "url": "/clicky-game/static/js/main.d83250e0.chunk.js"
+  },
+  {
+    "revision": "fb9f72c6d9bfc22bcecb",
+    "url": "/clicky-game/static/js/1.fb9f72c6.chunk.js"
+  },
+  {
+    "revision": "d83250e02c17b2da2ba3",
+    "url": "/clicky-game/static/css/main.32a3d6ed.chunk.css"
+  },
+  {
+    "revision": "65bb07d550a27078ca411fb2791afaae",
+    "url": "/clicky-game/index.html"
+  }
+];
